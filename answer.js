af0bf8bc482a232f1fcb70261eacb5734b99a76a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-21 14:21:24 +0530

    solved the program in JS

---
+++ answer.js
@@ -0,0 +1,23 @@
+function twoSums(nums, target) {
+  var diff = "";
+  var output = [];
+  for (let i = 0; i < nums.length; i++) {
+    //subtract the target from each array entry
+    diff = target - nums[i];
+    console.log("diff--->", target, "-", nums[i], ["index->", i], "==", diff);
+
+    //if the difference is included in the array and the index of differance is not equal to the index of operatied entry,
+    if (nums.includes(diff) && nums.indexOf(diff) !== nums.indexOf(nums[i])) {
+      //get the index
+      console.log("index of differance-->", nums.indexOf(diff));
+      console.log("index of op-->", nums.indexOf(nums[i]));
+
+      //push the index/indices in the ouput array
+      output.push(nums.indexOf(diff));
+      console.log("o/p-->", output.sort());
+    }
+  }
+  return output;
+}
+
+console.log("--->", twoSums([5, 10], 10));
