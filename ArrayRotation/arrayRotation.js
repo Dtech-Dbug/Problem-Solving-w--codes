15375d5ad54ab1b2d3282aa6e637ff68d73b16a3
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-25 22:41:02 +0530

    rotation left functionality

--- ArrayRotation/arrayRotation.js
+++ ArrayRotation/arrayRotation.js
@@ -1,1 +1,14 @@
-let arr = [1, 2, 3, 4, 5, 6, 7];
+const leftShift = (array, shiftIndices) => {
+  let indices = shiftIndices;
+
+  for (j = 0; j < indices; j++) {
+    for (i = 0; i < 1; i++) {
+      var shiftedDigits = array.splice(i, 1);
+      array.push(shiftedDigits[0]);
+    }
+  }
+
+  console.trace(array);
+};
+
+leftShift([1, 2, 3, 4, 5, 6], 2);
