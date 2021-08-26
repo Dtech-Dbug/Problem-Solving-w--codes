248bf568943dcad67b8ca99dc931da6b1325c6dc
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-26 11:15:13 +0530

    update : arrayRotation.js

--- ArrayRotation/arrayRotation.js
+++ ArrayRotation/arrayRotation.js
@@ -1,14 +1,31 @@
-const leftShift = (array, shiftIndices, shiftdirection) => {
+// *shift function
+// ! params : array , shiftIndices , shiftDirection [by default, set to left shift]
+// ? pass a string ->'right' in the function params for right shift
+
+const shift = (array, shiftIndices, shiftdirection) => {
   let indices = shiftIndices;
 
-  for (j = 0; j < indices; j++) {
-    for (i = 0; i < 1; i++) {
-      var shiftedDigits = array.splice(i, 1);
-      array.push(shiftedDigits[0]);
+  // ? if shifTdirection is 'right' make a right shift of the indices specified
+  if (shiftdirection === "right") {
+    for (j = 0; j < indices; j++) {
+      for (i = array.length - 1; i < array.length; i++) {
+        var shiftedDigits = array.splice(i, 1);
+        array.unshift(shiftedDigits[0]);
+      }
     }
   }
 
-  console.trace(array);
+  // ? ele left shift , default
+  else {
+    for (j = 0; j < indices; j++) {
+      for (i = 0; i < 1; i++) {
+        var shiftedDigits = array.splice(i, 1);
+        array.push(shiftedDigits[0]);
+      }
+    }
+  }
+
+  return array;
 };
 
-leftShift([1, 2, 3, 4, 5, 6], 2);
+module.exports = shift;
