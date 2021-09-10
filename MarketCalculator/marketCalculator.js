27f3c894a4a2656f6d2e0b6c422f0b46451ef96a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-10 11:53:26 +0530

    docs update : working code w/ comments

--- MarketCalculator/marketCalculator.js
+++ MarketCalculator/marketCalculator.js
@@ -1,8 +1,30 @@
 // TODO : a function that returns calculations
-// ? params : itemName, item price/kg, item quantity
+// ? params : itemName, item price/kg, item quantity, discount(optional)
 
-const priceCalculator = (itemName, unitPrice, quantity) => {
-  console.log(itemName, unitPrice, quantity);
+const priceCalculator = (itemName, unitPrice, quantity, discount) => {
+  let total;
+
+  total = unitPrice * quantity;
+
+  let list = {
+    name: itemName,
+    price: unitPrice,
+    qty: quantity,
+    total: total,
+  };
+
+  // * check if discount is undefined or null
+  if ((discount === undefined) | (discount === null)) {
+    return list;
+  } else {
+    let discountedPrice = (total * discount) / 100;
+
+    let totalAfterDiscount = total - discountedPrice;
+
+    list.totalAfterDiscount = totalAfterDiscount;
+
+    return list;
+  }
 };
 
-priceCalculator("Bread", 20, 8);
+console.table(priceCalculator("Bread", 200, 8, 90));
