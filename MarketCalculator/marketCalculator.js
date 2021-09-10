b7ef392dbab5a0806abbfc6eeadd1b03322bf1c9
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-10 11:30:01 +0530

    trial :

--- MarketCalculator/marketCalculator.js
+++ MarketCalculator/marketCalculator.js
@@ -1,2 +1,8 @@
 // TODO : a function that returns calculations
 // ? params : itemName, item price/kg, item quantity
+
+const priceCalculator = (itemName, unitPrice, quantity) => {
+  console.log(itemName, unitPrice, quantity);
+};
+
+priceCalculator("Bread", 20, 8);
