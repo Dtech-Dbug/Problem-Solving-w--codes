fbae31973c932f8d820c0d3c381b48611ae07975
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-12 10:42:12 +0530

    update : factorial.js => code w/ recursion

--- Factorial/factorial.js
+++ Factorial/factorial.js
@@ -1,7 +1,12 @@
 // input : 4 :: 4 x 3 x 2 x 1
+//input : 0 :: o/p --> 1
 
 let factorial = (num) => {
-  CSSConditionRule.log(num);
+  // ! check if number is 0 , then return 1 if true
+  if (num === 0) {
+    return 1;
+  } else {
+    // * if positive, run a recurive function
+    return num * factorial(num - 1);
+  }
 };
-
-factorial(5);
