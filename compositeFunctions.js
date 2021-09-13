515c0c31a820e26a6a5ef08e87c8add57994074f
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-13 22:51:35 +0530

    update :compositeFunctions.js
    - imported numberMania

--- compositeFunctions.js
+++ compositeFunctions.js
@@ -8,6 +8,7 @@ const shift = require("./ArrayRotation/arrayRotation");
 const { reverseWord, mirrorWords } = require("./ReverseWord/reverseWord");
 const priceCalculator = require("./MarketCalculator/marketCalculator");
 const factorial = require("./Factorial/factorial");
+const numberMania = require("./Numbermania/numberMania");
 
 let compose = {
   twoSumsProblem: twoSums,
@@ -17,9 +18,11 @@ let compose = {
   mirrorWords,
   priceCalculator,
   factorial,
+  numberMania,
 };
 
 // console.log(compose.shift([1, 2, 3, 4, 5], 3));
 // console.log(compose.mirrorWords("Engineer"));
 // console.log(compose.priceCalculator("Eggs", 6, 20));
 // console.log(factorial(5));
+console.log(numberMania.isEven(6));
