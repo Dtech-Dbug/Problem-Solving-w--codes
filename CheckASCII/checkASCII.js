ac2f1c8134a99345a6d1336d677d25ff8a45037d
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-09-27 21:32:58 +0530

    traspiled js with tsc --watch

---
+++ CheckASCII/checkASCII.js
@@ -0,0 +1,5 @@
+var readlineSync = require("readline-sync");
+var userInput = readlineSync.question("char: \n");
+// console.log(userInput.charCodeAt(0)); length
+var LENGTh = userInput.length;
+console.log(LENGTh);
