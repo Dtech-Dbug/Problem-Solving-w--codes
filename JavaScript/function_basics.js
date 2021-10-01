2cf9f975801f61094049c7024833fd4e872549de
Sunaina Agarwal <56354115+Sunainacode@users.noreply.github.com>
2021-10-01 17:15:20 +0530

    Create function_basics.js

---
+++ JavaScript/function_basics.js
@@ -0,0 +1,21 @@
+console.log("This is a code for functions in javascript")
+let name="ABC"
+let name1="DEF"
+let name2="GHI"
+let name3="JKL"
+// to print a message with all the four names above would be tedious
+// console.log(name+ " is a good girl")
+// console.log(name1+ " is a good girl")
+// console.log(name2+ " is a good girl")
+// console.log(name3+ " is a good girl")
+
+//functions make it easy
+function gg(name){
+    console.log(name+ " is a good girl")
+
+}
+
+gg(name);
+gg(name1);
+gg(name2);
+gg(name3);
